ddb8f996d101be94fa1855616e823e4ec552b5bc
Harun Hurtic <harunhurtic@gmail.com>
2023-02-12 23:54:18 +0100

    Added link to tutorial for next working session

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 /*app.get('/', (req, res) => {
-  res.send('The server on notes app is working! And I'm testing it right now! :)')
+  res.send('The server on notes app is working! And I'm testing it right now! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)')
 })*/
 
 app.listen(port, () => {
