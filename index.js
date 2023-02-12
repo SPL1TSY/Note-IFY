d89616070c7cf09ba1f38672f014bca13c44e671
Harun Hurtic <harunhurtic@gmail.com>
2023-02-12 23:28:56 +0100

    Nothing special

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('The server on notes app is working! :)')
 })
 
 app.listen(port, () => {
