6edac31bb68245d0ffc2f860db9653c12feff3aa
Harun Hurtic <harunhurtic@gmail.com>
2023-02-12 23:57:44 +0100

    Added some comments till next working session

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = 3000;
 app.use(express.static('public'));
 
 /*app.get('/', (req, res) => {
-  res.send('The server on notes app is working! And I'm testing it right now! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)')
+  res.send('The server on notes app is working! And I'm testing it right now! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)
+  Also remember to test server out by "npm run dev" in package.json')
 })*/
 
 app.listen(port, () => {
