c315fd9ed9696c42f0491d4f04d132462019228f
Harun Hurtic <harunhurtic@gmail.com>
2023-02-12 23:41:21 +0100

    Started the process of server and app

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+/*app.get('/', (req, res) => {
   res.send('The server on notes app is working! :)')
-})
+})*/
 
 app.listen(port, () => {
   console.log(`Notes app listening on port http://localhost:${port}`)
