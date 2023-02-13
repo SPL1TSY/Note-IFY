1ebcf8189e4e8432ce09350eb34e9ebcf52e1731
Harun Hurtic <harunhurtic@gmail.com>
2023-02-13 15:24:42 +0100

    Made changes to comments in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 /*app.get('/', (req, res) => {
   res.send('The server on notes app is working! And I'm testing it right now! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)
-  Also remember to test server out by "npm run dev" in package.json')
+  Also remember to test server out by "npm run dev" in package.json' -> "npm install -g nodemon" to install nodemon on mac)
 })*/
 
 app.listen(port, () => {
