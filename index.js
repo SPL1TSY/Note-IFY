d800809f2474d887cf71850e610eb70f43ddd0e6
Harun Hurtic <harunhurtic@gmail.com>
2023-02-14 12:48:32 +0100

    Test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 8080;
 app.use(express.static('public'));
 
 /*app.get('/', (req, res) => {
-  res.send('The server on notes app is working! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)
+  res.send('The server on notes app is working! Test. Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)
   Also remember to test server out by "npm run dev" in package.json' -> "npm install -g nodemon" to install nodemon on mac)
 })*/
 
