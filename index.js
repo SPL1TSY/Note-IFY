901471a50e443d180a7bef380e1c33bebae4fe5d
Harun Hurtic <harunhurtic@gmail.com>
2023-02-16 23:49:30 +0100

    Deleted some files & updated some text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -11,13 +11,20 @@ app.listen(port, () => {
   console.log(`Notes app listening on port http://localhost:${port}`)
 }); 
 
-app.get ('/', async (request, response) => {
-  //run code in here and stuff98765r4e3w2q1
-  response.send ('Server is running');
+app.get ('/', async (req, res) => {
+  //run code in here and stuff
+  res.send ('Server is running');
+
 });
 
 /*
-app.get('/', (req, res) => {
-  res.send('The server on notes app is working! Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY :)
-  Also remember to test server out by "npm run dev" in package.json' -> "npm install -g nodemon" to install nodemon on mac)
-})*/
+  Notes to myself:
+  Continue watching this tut, about 5mins in: https://www.youtube.com/watch?v=wYALykLb5oY 
+  Also use this one from Geeksforgeeks //specifically used for making a notes app: https://www.geeksforgeeks.org/how-to-build-note-taking-application-using-node-js/ 
+  https://github.com/viakeegan/notes-are-kee/blob/main/server.js
+  https://blog.devgenius.io/learning-nodejs-by-creating-a-notes-app-9eafc4223306
+
+  npm install express ejs body-parser
+
+  Also remember to test server out by "npm run dev" in package.json' (install nodemon for refresh of server for every change via -> "npm install -g nodemon" nodemon on mac)
+*/
